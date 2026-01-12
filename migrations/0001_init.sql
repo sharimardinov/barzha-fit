08433257468aa3279213abc5089139e89a1e6f43
sharimardinov <sharimardinov@icloud.com>
2026-01-12 17:54:46 +0500

    sql fix

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -3,3 +3,19 @@ create table if not exists plans (
                                      text       text not null,
                                      created_at timestamptz not null default now()
 );
+
+create table if not exists users (
+                                     user_id    bigint primary key,
+                                     cycle_day  int not null default 1,
+                                     created_at timestamptz not null default now(),
+                                     updated_at timestamptz not null default now()
+);
+
+create table if not exists workout_days (
+                                            user_id    bigint not null,
+                                            day_date   date not null,
+                                            cycle_day  int not null,
+                                            status     text not null, -- 'done' | 'skip'
+                                            created_at timestamptz not null default now(),
+                                            primary key (user_id, day_date)
+);
